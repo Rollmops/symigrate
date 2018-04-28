2dcee86b6f5807f663b7719e52fa2cbd0ab7f28b
Erik Tuerke <etuerke@googlemail.com>
2018-04-28 20:36:53 +0200

    setup.py cosmetics

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup, find_packages
 
 from symigrate import __version__
 
-setup(name="symigrate",
-      version=__version__,
-      packages=find_packages(),
-      entry_points={
-          "console_scripts": [
-              "symigrate=symigrate.main.symigrate:main"
-          ]
-      },
-      install_requires=[
-          "tabulate"
-      ]
-      )
+setup(
+    name="symigrate",
+    version=__version__,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "symigrate=symigrate.main.symigrate:main"
+        ]
+    },
+    install_requires=[
+        "tabulate"
+    ]
+)
