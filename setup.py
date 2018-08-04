02ccd704f418a400e802f93e86069023fd56a820
Erik Türke <etuerke@googlemail.com>
2018-08-04 15:58:02 +0200

    cosmetics

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@ from setuptools import setup, find_packages
 
 from symigrate import __version__
 
+DESCRIPTION = """A flyway-like tool that allows migration of scripts to a system."""
+
 setup(
     name="symigrate",
     version=__version__,
+    description=DESCRIPTION,
+    author="Erik Tuerke",
+    author_email="etuerke@googlemail.com",
+    license="Apache2",
+    classifiers=[
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console"
+    ],
     packages=find_packages(),
     entry_points={
         "console_scripts": [
