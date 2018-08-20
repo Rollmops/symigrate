ecb89d20e148a03db553e3d90b36c019807f487f
Erik Türke <etuerke@googlemail.com>
2018-08-20 19:40:00 +0200

    add first travis config

--- symigrate/migration_script_checker.py
+++ symigrate/migration_script_checker.py
@@ -7,6 +7,8 @@ LOGGER = logging.getLogger(__name__)
 
 
 class MigrationScriptChecker:
+    os_access_func_hook = os.access
+
     @staticmethod
     def check(file_path: str):
         MigrationScriptChecker._check_permissions(file_path)
@@ -14,7 +16,7 @@ class MigrationScriptChecker:
     @staticmethod
     def _check_permissions(file_path: str):
         LOGGER.debug("Checking execution permissions for: %s", file_path)
-        if not os.access(file_path, os.X_OK):
+        if not MigrationScriptChecker.os_access_func_hook(file_path, os.X_OK):
             raise MigrationScriptChecker.MigrationScriptPermissionException(
                 "Migration script '{file_path}' is not executable".format(file_path=file_path)
             )
